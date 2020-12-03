d18b1f8ea7d681fb14256b4d43d7bffe014433ca
Christian Carnalla <ccarnalla4514@stu.d214.org>
2020-12-03 14:56:15 -0600

    Initial Commit

---
+++ ConditionalsLecture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ConditionalsLecture
+//
+//  Created by Christian Carnalla on 12/3/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
